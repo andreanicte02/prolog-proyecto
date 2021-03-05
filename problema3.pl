9cb01a419111e7d9e7bc0d95525d5b5ac6c6c2af
andreanicte02 <andreanicte02@gmail.com>
2021-03-05 17:46:06 -0600

    -dividr y polindrome

--- problema3.pl
+++ problema3.pl
@@ -1,21 +1,27 @@
 
 %swipl -s problema3.pl
 
+%repetir(ListaRepetida,Lista)
+repetir([],[]).
+repetir([Cabeza,Cabeza|RestoCola],[Cabeza|Cola]) :- repetir(RestoCola,Cola).
 
-repetir_lista([],[]).
-repetir_lista([Cabeza|Cola],[Cabeza,Cabeza|RestoCola]) :- repetir_lista(Cola,RestoCola).
+%voltear(Lista,ListaVolteada)
+voltear([],[]).
+voltear([Cabeza|Cola], ListaVolteada):- voltear(Cola, ColaVolteada),
+                                              juntar(ColaVolteada,ListaVolteada,[Cabeza]).                                
+juntar([],Lista,Lista).
+juntar([Cabeza1|Lista1],[Cabeza1|Lista3],Lista2):-juntar(Lista1,Lista3,Lista2).
 
-
-voltear_lista([],[]).
-voltear_lista([Cabeza|Cola], ListaVolteada):- voltear_lista(Cola, ColaListaVolteada),
-                                              juntar_listas(ColaListaVolteada,[Cabeza],ListaVolteada).                                
-juntar_listas([],Lista,Lista).
-juntar_listas([Cabeza1|Cola1],Cola2,[Cabeza1|Cola4]):-juntar_listas(Cola1,Cola2,Cola4).
-
-
-polindrome(Lista):- voltear_lista(Lista,Lista).
+%se mandan la misma lista como la que se recibe y la que se obtiene al voltearla
+%si son iguales es verdadero
+polindrome(Lista):- voltear(Lista,Lista).
 
 %dividir(List,List2,List3)
 dividir([],[],[]).
 dividir([Elemento1],[Elemento1],[]).
 dividir([Elemento1,Elemento2|Lista],(Elemento1|Lista1), [Elemento2|Lista2]):-dividir(Lista,Lista1,Lista2).
+
+%insertar
+insertar(X,1,L,[X|L]):-!.
+insertar(X,Index,[Cabeza|Lista1],[Cabeza,Lista2]):-K is Index-1, 
+                                insertar(X,K,Lista1,Lista2).
